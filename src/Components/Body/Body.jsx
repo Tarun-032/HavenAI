cf13413ae3771a7304d67006c25e354aac661ecd
Tarun Sudhir Behera <99060004+Tarun-032@users.noreply.github.com>
2024-08-16 01:47:53 +0530

    body changes

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import './body.css'
+import { FaMicrophone } from 'react-icons/fa';
+import { useState } from 'react';
+
+
+
+const handleMicClick = () => {
+  // Implement voice recording logic here
+  setCaptions('Listening...');
+};
 
 const Body = () => {
+
+  const [captions, setCaptions] = useState('');
+
   return (
        <main className="content">
-        <h1>Emotionally intelligent voice AI for everyone</h1>
+        <h1>Emotionally Intelligent Voice AI for Everyone</h1>
         <p>Just ask anything related to your health or mental health and the AI will respond to you accordingly</p>
-        {/* Add more content here */}
+        <div className="mic-button" onClick={handleMicClick}>
+        <FaMicrophone />
+      </div>
+      <p class= "para">Click here</p>
+      <div className="captions">
+        {captions}
+      </div>
       </main>
   )
 }
